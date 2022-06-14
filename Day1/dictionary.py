10bfba75cd1df7b6f24085f957d03b03d38d7057
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-06-14 16:24:20 +0530

    Add files via upload

---
+++ Day1/dictionary.py
@@ -0,0 +1,11 @@
+d={"Name":"Paul Thomas","Age":20,"Dob":"2002/25/4"}
+print(d)
+print(d["Name"])
+
+x={"Name":{"Firstname":"KK","SecondName":"Haridev"},"Age":20}
+print(x)
+print(x["Name"])
+print(x["Name"]["Firstname"])
+y={"Name":["Manu","Sam","Vinu"],"Age":[20,30,40]}
+print(y)
+print(y["Name"][1])
